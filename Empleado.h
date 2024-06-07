6c029045c4ca27a29c275b930e98356ab09c264a
RicardoRuiz2005 <150260615+RicardoRuiz2005@users.noreply.github.com>
2024-06-07 01:12:50 -0600

    Update Empleado.h

--- Empleado.h
+++ Empleado.h
@@ -1,89 +1,89 @@
+/*Este codigo define clases de empleados, como Operador, Director y 
+  Jefe, que heredan de la clase base Empleado. Cada clase tiene metodos para 
+  trabajar, pero con comportamientos especificos: el Operador trabaja en 
+  produccion, el Director dirige la produccion y el Jefe dirige la empresa.*/
+
 #ifndef EMPLEADO_H
 #define EMPLEADO_H
 
-#include<iostream>
-#include<string>
-
+#include <iostream>
+#include <string>
 using namespace std;
 
-//Clase padre de Empleado
-class Empleado{
-	protected:
-		string nombreEmpleado;
-		string puesto;
-		string matricula;
-	
-	public:
-		Empleado() : nombreEmpleado(""), puesto(""), matricula("") {}
-		Empleado(string nom, string pues, string matri) : nombreEmpleado(nom), puesto(pues), matricula(matri) {}
-		
-		//Getters
-		string getNombreEmpleado(){
-			return nombreEmpleado;
-		}
-		string getPuesto(){
-			return puesto;
-		}		
-		string getMatricula(){
-			return matricula;
-		}
-		
-		//Setters
-		void setNombreEmpleado(string nombre){
-			nombreEmpleado = nombre;
-		}
-		void setPuesto(string puest){
-			puesto = puest;
-		}
-		void setMatricula(string matricu){
-			matricula = matricu;
-		}
-		
-		//Metodos
-		virtual void mostrarDatos(){
-			cout<<"El empleado "<<matricula<<" se llama: "<<nombreEmpleado<<endl;
-			cout<<"Su puesto en la empesa es: "<<puesto<<endl;
-		}
-		
-		virtual void trabajar(){
-			cout<<"Este empleado esta trabajando"<<endl;
-		}
+class Empleado {
+protected:
+    string nombreEmpleado;
+    string puesto;
+    string matricula;
+
+public:
+    Empleado() : nombreEmpleado(""), puesto(""), matricula("") {}
+    Empleado(string nom, string pues, string matri) : nombreEmpleado(nom), puesto(pues), matricula(matri) {}
+
+    // Getters
+    string getNombreEmpleado() {
+        return nombreEmpleado;
+    }
+    string getPuesto() {
+        return puesto;
+    }
+    string getMatricula() {
+        return matricula;
+    }
+
+    // Setters
+    void setNombreEmpleado(string nombre) {
+        nombreEmpleado = nombre;
+    }
+    void setPuesto(string puest) {
+        puesto = puest;
+    }
+    void setMatricula(string matricu) {
+        matricula = matricu;
+    }
+
+    // Métodos
+    virtual void mostrarDatos() {
+        cout << "El empleado " << matricula << " se llama: " << nombreEmpleado << endl;
+        cout << "Su puesto en la empresa es: " << puesto << endl;
+    }
+
+    virtual void trabajar() {
+        cout << "Este empleado esta trabajando" << endl;
+    }
 };
 
-//Clase hija de Empleado (Operador)
-class Operador : public Empleado{
-	public:
-		Operador() : Empleado() {}
-		Operador(string nom, string pues, string matri) : Empleado(nom, pues, matri) {}		
-	
-		void trabajar(){
-			cout<<"Este empleado esta trabajando"<<endl;
-			cout<<"El operador: "<<nombreEmpleado<<" de matricula: "<<matricula<<" esta trabajando en la produccion: ---insertar---"<<endl;
-		}
+class Operador : public Empleado {
+public:
+    Operador() : Empleado() {}
+    Operador(string nom, string pues, string matri) : Empleado(nom, pues, matri) {}
+
+    void trabajar() {
+        cout << "Este empleado esta trabajando" << endl;
+        cout << "El operador: " << nombreEmpleado << " de matricula: " << matricula << " esta trabajando en la produccion" << endl;
+    }
 };
 
-//Clase hija de Empleado (Director)
-class Director : public Empleado{
-	public:
-		Director() : Empleado() {}
-		Director(string nom, string pues, string matri) : Empleado(nom, pues, matri) {}		
-	
-		void trabajar(){
-			cout<<"Este empleado esta dirijiendo"<<endl;
-			cout<<"El trabajador: "<<nombreEmpleado<<" de matricula: "<<matricula<<" esta trabajando en la produccion: ---insertar---"<<endl;
-		}
+class Director : public Empleado {
+public:
+    Director() : Empleado() {}
+    Director(string nom, string pues, string matri) : Empleado(nom, pues, matri) {}
+
+    void trabajar() {
+        cout << "Este empleado esta dirigiendo" << endl;
+        cout << "El trabajador: " << nombreEmpleado << " de matricula: " << matricula << " esta trabajando en la produccion" << endl;
+    }
 };
 
-//Clase hija de Empleado (Jefe)
-class Jefe : public Empleado{
-	public:
-		Jefe() : Empleado() {}
-		Jefe(string nom, string pues, string matri) : Empleado(nom, pues, matri) {}		
-	
-		void trabajar(){
-			cout<<"Este es el jefe"<<endl;
-			cout<<"El jefe tiene como nombre: "<<nombreEmpleado<<" con matricula: "<<matricula<<" esta dirijiendo la empresa"<<endl;
-		}
+class Jefe : public Empleado {
+public:
+    Jefe() : Empleado() {}
+    Jefe(string nom, string pues, string matri) : Empleado(nom, pues, matri) {}
+
+    void trabajar() {
+        cout << "Este es el jefe" << endl;
+        cout << "El jefe tiene como nombre: " << nombreEmpleado << " con matricula: " << matricula << " esta dirigiendo la empresa" << endl;
+    }
 };
 
 #endif
